b68e76a25cfed5b475e60590370d596832cdb9a7
nwokolouser <115836687+nwokolouser@users.noreply.github.com>
2022-10-14 15:59:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// HAMBURGER MENU JS
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+const ul = document.querySelector("ul");
+
+const navigationBar = document.querySelector(".navigation-list");
+
+const showMenu = document.querySelector(".show-navbar");
+
+hamburgerMenu.addEventListener("click", () => {
+    navigationBar.classList.toggle("show-navbar");
+    showMenu.classList.toggle("ul")
+});
+
+
+// RESERVATION JS
+
+let locationInput = document.querySelector("#location");
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+
+    (e).preventDefault();
+});
+
+
+
+
+// LEE-BIO JS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
